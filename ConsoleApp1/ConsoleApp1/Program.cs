38603f2a5135755a306a8dfce42b8b075b7a7d79
EirinaiosAbb <62116406+EirinaiosAbb@users.noreply.github.com>
2020-03-12 21:34:02 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("give your num");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("give your second num");
+            double b = Convert.ToDouble(Console.ReadLine());
+            if (a < b)
+            {
+                Console.WriteLine(a <= b);
+
+            }
+            
+        }
+    }
+
+}
